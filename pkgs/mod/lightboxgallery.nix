199d9c2f8573d48e24dfd1edf1ad8146c60d3292
Finn Behrens <me@kloenk.dev>
2022-11-02 10:07:53 +0100

    Update lightbox

--- pkgs/mod/lightboxgallery.nix
+++ pkgs/mod/lightboxgallery.nix
@@ -4,8 +4,8 @@ moodle-utils.buildMoodlePlugin {
     name = "lightboxgallery";
     src = fetchzip {
         name = "lightboxgallery";
-        url = "https://moodle.org/plugins/download.php/26901/mod_lightboxgallery_moodle40_2022062400.zip";
-        sha256 = "sha256-eyFOQzx+uXx/VEDtfM4WjE6D0hmBnqfAZtStr261BMo=";
+        url = "https://moodle.org/plugins/download.php/27813/mod_lightboxgallery_moodle40_2022102800.zip";
+        sha256 = "sha256-mYbnmL0e7u08ynmO7unW0cDAlyAQRuDzah1k1/PTebY=";
         extraPostFetch = ''
           echo fix permissions: $out
           chmod a-w -R $out
