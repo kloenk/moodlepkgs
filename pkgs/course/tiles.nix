cd3e1be87615e9a2b525cc199cfc8925131ae6fc
Finn Behrens <me@kloenk.dev>
2023-03-02 11:09:21 +0100

    fix sha

--- pkgs/course/tiles.nix
+++ pkgs/course/tiles.nix
@@ -5,7 +5,7 @@ moodle-utils.buildMoodlePlugin {
   src = fetchzip {
     name = "tiles";
     url = "https://moodle.org/plugins/download.php/28650/format_tiles_moodle41_2023022800.zip";
-    sha256 = "sha256-e+DuRxckigBfg6SyWHTPxt7bm0ZkQDHGK6CYXZXlxa8=";
+    sha256 = "sha256-EEdPozz1cXbxPqOjjBeRfvZE+hyO/GEH+gfDhwAJWhg=";
     extraPostFetch = ''
       echo fix permissions: $out
       chmod a-w -R $out
