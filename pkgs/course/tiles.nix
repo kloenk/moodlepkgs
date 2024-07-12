c73d62813739bfdb935591e6a565a23dc0530a2b
Finn Behrens <me@kloenk.de>
2024-07-12 12:49:57 +0200

    update tiles

--- pkgs/course/tiles.nix
+++ pkgs/course/tiles.nix
@@ -4,8 +4,8 @@ moodle-utils.buildMoodlePlugin {
   name = "tiles";
   src = fetchzip {
     name = "tiles";
-    url = "https://moodle.org/plugins/download.php/32425/format_tiles_moodle42_2024062501.zip";
-    sha256 = "sha256-njdRr4WjemDbmGaroEydhmWpRhNrRohQsm165WdhyOI=";
+    url = "https://moodle.org/plugins/download.php/32549/format_tiles_moodle44_2024071107.zip";
+    sha256 = "sha256-S3IBc+jQwxRVuvATIDhAO1hQuOlOIDeyMbh7ZMhezL8=";
     extraPostFetch = ''
       echo fix permissions: $out
       chmod a-w -R $out
